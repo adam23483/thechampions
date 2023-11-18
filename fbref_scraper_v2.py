4e17a7294ee26581a0c3b1ff0b51e037571ed60e
Adam^2 <63423355+adam23483@users.noreply.github.com>
2023-11-18 18:25:11 -0500

    finding bast cases for fuctions

--- fbref_scraper_v2.py
+++ fbref_scraper_v2.py
@@ -47,69 +47,91 @@ data_stats_ids  = [
     "matches"
 ]
 player_stats = {"player":[]}
-
-
-
-#finds standard stats table on fbref
-full_table= soup.find(id= 'stats_standard')
-
-#removes standard stats table header
-for sh in soup.select('tr.thead'):
-  sh.decompose()
-
-#removes mid row headers form standard stats table
-for th in full_table.find_all("tr"):
-  th.th.decompose()
-#removes match column form standard stats table
-for match in full_table.find("tbody").find_all('td',string='Matches'):
-  match.decompose()
-  
 #loop for getting stats then adding to data_stat array 
   #1)gets tag id from list 
   #2)create text for .select() search
   #3)adds to array
 
-for data_row in full_table.find('tbody').select("tr", class_="data-row="):
-  for stat_id in iter(data_stats_ids):
-    selected_id = 'td[data-stat="'+ stat_id +'"]'
-    all_data_rows = data_row.select(selected_id)
-  
-  if stat_id == "player":
-    for player_data in all_data_rows:
-      name_siblings = player_data.find_next_siblings()
-      name = player_data.get_text()
-      player_stats["player"].append(name) 
-      print(name)
-      for k in name_siblings:
-      
-      
-       
 
-        
+#finds standard stats table on fbref
+full_table= soup.find(id= 'stats_standard')
 
-        for x in name_siblings:
-          if all_data_rows != None:
-            info = x.get_text()
-            z = str(stat_id + " : " + info)
-            print(z)
-
-          
-        
-          
-            
-          
-
-        
-
-
-
-
-'''
- for x in name_siblings:
-          
-            for y in all_data_td:
-               '''
+#one function for all decompose requests 
+#removes standard stats: header, mid row headers, and match colomn
+def decompose_items():
+  for x in soup.select('tr.thead'):
+        x.decompose()
+  for x in full_table.find_all("tr"):
+        x.th.decompose()
+  for x in full_table.find("tbody").find_all('td',string='Matches'):
+        x.decompose()
+decompose_items()
  
+def stat_id_selector(data_stats_ids):
+    selected_id =[]
+    for stat_id in data_stats_ids:
+        selected_id = 'td[data-stat="'+  stat_id +'"]'
+
+        
+      
+
+stat_id_selector(data_stats_ids)
+
+
+    
+def id_selector(stat_id):
+  stat_id_selector()
+  
+  return selected_id
+
+"""def player_stat_creator(selected_id):
+  id_selector()
+  for data_row in full_table.find('tbody').select("tr", class_="data-row="):
+    current_data = data_row.select(selected_id)
+    return current_data
+  
+def player_finder(current_data,stat_id):
+    player_stat_creator()
+    if stat_id == "player":
+       player_stats["player"].append(current_data.get_text())
+       name_siblings = current_data.find_next_siblings()
+    return name_siblings
+
+def find_data(name_siblings,stat_id,selected_id):
+  player_finder()
+  for this_player in name_siblings:
+     player_stat_creator(name_siblings)
+
+     player_stats["player"].append(stat_id + ":" +)
+     player_stats["player"][stat_id].append()  
+if stat_id_selector() == ""
+def data_id_tag():
+  for data in
+def id_matcher():    
+  for stat_id_fucntion in iter(data_stats_ids):
+    selected_id = 'td[data-stat="'+ stat_id_fucntion +'"]'
+
+      return all_data_rows
+
+def player_row_data():
+  for player_data in id_matcher:
+    
+  return name_siblings
+
+print(all_data_rows)
+
+if id_matcher(stat_id_fuctio)
+
+    name = player_data.get_text()
+    player_stats["player"].append(name) 
+    print(name)
+
+          for x in name_siblings:
+        if all_data_rows != None:
+          info = x.get_text()
+          z = str(stat_id + " : " + info)
+          print(z)
+
 
 #notes for me 
 # 1) try out getting the data-row tag should help orgonzie data
@@ -119,3 +141,4 @@ for data_row in full_table.find('tbody').select("tr", class_="data-row="):
 
 
 
+"""
---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import array as arr
+
+url = 'https://fbref.com/en/comps/9/stats/Premier-League-Stats'
+response = requests.get(url)
+html_content = response.text.replace('<!--', '').replace('-->', '') #.replace() removes player data comment out  
+soup = BeautifulSoup(html_content, 'lxml')
+
+full_table= soup.find(id= 'stats_standard')
+     
+print(full_table.get_text())
+
+
